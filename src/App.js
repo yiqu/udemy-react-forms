16003c7508bc75c3dd57002550542fdada5e260a
Yi Qu yqu <yqu@ip-10-113-132-29.evoforge.org>
2022-09-30 13:35:37 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+
+  const title = 'Forms!'
+
+  
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
